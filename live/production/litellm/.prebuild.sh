485b6357c338f44565adf23fc6fccba392ccca49
vladimir100m <vlad@ubika.com>
2026-03-11 23:44:08 -0300

    deploy

--- live/production/litellm/.prebuild.sh
+++ live/production/litellm/.prebuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo ".prebuild.sh is deprecated. Running .postbuild.sh instead."
-exec ./.postbuild.sh
+echo "Force rebuild of LiteLLM layer by exiting with code 0 and skipping the rest of the script"
+
+exit 0
