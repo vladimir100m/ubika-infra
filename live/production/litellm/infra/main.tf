8e7d7fad7b716ab885128aa51069b4e5f25eb17a
vladimir100m <vlad@ubika.com>
2026-03-16 18:40:33 -0300

    se actualiza worflow from cursor

--- live/production/litellm/infra/main.tf
+++ live/production/litellm/infra/main.tf
@@ -239,6 +239,7 @@ resource "aws_secretsmanager_secret_version" "llm_api_keys" {
   secret_id = aws_secretsmanager_secret.llm_api_keys.id
   secret_string = jsonencode({
     GEMINI_API_KEY       = var.gemini_api_key
+    LANGFUSE_SECRET_KEY  = var.langfuse_secret_key
   })
 }
 
